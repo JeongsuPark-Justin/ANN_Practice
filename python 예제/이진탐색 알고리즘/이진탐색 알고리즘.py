4d86b72f07cbdd5fb70fe8af22ef6eab8945daa6
Justin <jungsoo1016@naver.com>
2021-08-02 22:42:53 +0900

    python tutorial (예제)

---
+++ python 예제/이진탐색 알고리즘/이진탐색 알고리즘.py
@@ -0,0 +1,34 @@
+# 이진탐색 알고리즘
+# 순차탐색 : 리스트 안에 있는 특정한 데이터를 찾기 위해 앞에서부터 데이터를 하나씩 확인하는 방법
+# 이진탐색 : 정렬되어 있는 리스트에서 탐색 범위를 절반씩 좁혀가며 데이터를 탐색하는 방법
+#           이진탐색은 시작점, 끝점, 중간점 (인덱스) 을 이용하여 탐색 범위를 설정합니다.
+
+# --> 탐색 범위를 절반씩 줄이며 O(logN)
+# 정렬되어있는 array를 입력
+
+# 이진 탐색 소스코드 : (재귀함수)
+def binary_search(array, target, start, end):
+    if start > end:
+        return None
+    mid = (start + end) // 2
+    # 찾은 경우 중간점 인덱스 반환
+    if array[mid] == target:
+        return mid
+    # 중간점의 값보다 찾고자 하는 값이 작은 경우 왼쪽 확인
+    elif array[mid] > target:
+        return binary_search(array, target, start, mid - 1)
+    # 중간점의 값보다 찾고자 하는 값이 큰 경우 오른쪽 확인
+    else:
+        return binary_search(array, target,  mid + 1, end)
+
+# n(원소의 개수)과 target(찾고자 하는 값)을 입력 받기
+n, target = list(map(int, input().split()))
+# 전체 원소 입력받기
+array = list(map(int, input().split()))
+
+# 이진 탐색 수행 결과 출력
+result = binary_search(array, target, 0, n - 1)
+if result == None:
+    print("원소가 존재하지 않습니다")
+else:
+    print(result + 1)
